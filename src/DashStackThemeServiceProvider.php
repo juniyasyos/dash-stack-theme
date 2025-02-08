066d152ac1604bc5d0631b335e2772f88ae71f1b
Fa-BRAIK <faroukbraik17@gmail.com>
2025-02-08 13:21:24 -0500

    initial commit

---
+++ src/DashStackThemeServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nuxtifyts\DashStackTheme;
+
+use Illuminate\Support\ServiceProvider;
+
+class DashStackThemeServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        
+    }
+}
