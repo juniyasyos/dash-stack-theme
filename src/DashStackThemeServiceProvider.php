f57528b4a123d97befeb5c78e690e19000986493
Fa-BRAIK <faroukbraik17@gmail.com>
2025-02-08 13:31:14 -0500

    added configuration for the package

--- src/DashStackThemeServiceProvider.php
+++ src/DashStackThemeServiceProvider.php
@@ -6,8 +6,22 @@ use Illuminate\Support\ServiceProvider;
 
 class DashStackThemeServiceProvider extends ServiceProvider
 {
+    protected const CONFIG_FILE_NAME = 'filament-dash-stack-theme';
+
+    protected const CONFIG_FILE_NAME_FULL = self::CONFIG_FILE_NAME.'.php';
+
     public function register(): void
     {
-        
+        $this->mergeConfigFrom(path: $this->configFilePath(), key: self::CONFIG_FILE_NAME);
+
+        $this->publishes(
+            paths: [$this->configFilePath() => config_path(self::CONFIG_FILE_NAME_FULL)],
+            groups: 'filament-dash-stack-theme-config'
+        );
+    }
+
+    private function configFilePath(): string
+    {
+        return __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.self::CONFIG_FILE_NAME_FULL;
     }
 }
