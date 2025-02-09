3f0c100b78ac2fe064511be57b0c6567af9db7ec
Fa-BRAIK <faroukbraik17@gmail.com>
2025-02-09 11:38:58 -0500

    refactor: improve package configuration and command registration in DashStackThemeServiceProvider

--- src/DashStackThemeServiceProvider.php
+++ src/DashStackThemeServiceProvider.php
@@ -5,17 +5,30 @@ namespace Nuxtifyts\DashStackTheme;
 use Nuxtifyts\DashStackTheme\Commands\FilamentDashStackThemeInstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Console\Command;
 
 class DashStackThemeServiceProvider extends PackageServiceProvider
 {
+    protected const PACKAGE_NAME = 'nuxtifyts/dash-stack-theme';
+
     protected const CONFIG_FILE_NAME = 'filament-dash-stack-theme';
 
     public function configurePackage(Package $package): void
     {
         $package
-            ->name('nuxtifyts/dash-stack-theme')
+            ->name(self::PACKAGE_NAME)
             ->hasConfigFile(self::CONFIG_FILE_NAME)
             ->hasViews()
-            ->hasCommand(FilamentDashStackThemeInstallCommand::class);
+            ->hasCommands(...self::commandsClassStrings());
+    }
+
+    /** 
+     *  @return list<class-string<Command>>
+     */
+    protected static function commandsClassStrings(): array
+    {
+        return [
+            FilamentDashStackThemeInstallCommand::class
+        ];
     }
 }
